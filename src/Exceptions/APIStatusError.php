43613bd5e98720c922e885ec2a2dd12c0840b090
bytestream <kieran@supportpal.com>
2024-12-12 18:37:32 +0000

    wip

--- src/Exceptions/APIStatusError.php
+++ src/Exceptions/APIStatusError.php
@@ -2,20 +2,34 @@
 
 namespace OpenAI\Exceptions;
 
+use OpenAI\Responses\Meta\MetaInformation;
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\ResponseInterface;
 
-class APIStatusError extends APIError
+class APIStatusError extends ErrorException implements OpenAIThrowable
 {
     protected int $statusCode;
     protected ?string $requestId;
 
-    public function __construct(RequestInterface $request, protected readonly ResponseInterface $response, array $contents)
-    {
-        parent::__construct($request, $contents);
+    public function __construct(
+        protected readonly RequestInterface $request,
+        protected readonly ResponseInterface $response,
+        array $contents
+    ) {
+        parent::__construct($contents, $response->getStatusCode());
 
         $this->statusCode = $response->getStatusCode();
         $this->requestId = $response->getHeader('x-request-id')[0] ?? null;
+    }
+
+    public function getRequest(): RequestInterface
+    {
+        return $this->request;
+    }
+
+    public function getResponse(): ResponseInterface
+    {
+        return $this->response;
     }
 
     public function getStatusCode(): int
@@ -26,5 +40,14 @@ class APIStatusError extends APIError
     public function getRequestId(): ?string
     {
         return $this->requestId;
+    }
+
+    public function meta(): ?MetaInformation
+    {
+        if (method_exists($this->response, 'getHeaders')) {
+            return MetaInformation::from($this->response->getHeaders());
+        }
+
+        return null;
     }
 }
